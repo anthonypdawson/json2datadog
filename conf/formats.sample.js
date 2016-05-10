a1219a77dad91fba1b9564ac453028dde2a2d358
Anthony Dawson <adawson@gsngames.com>
2016-05-10 17:17:59 -0400

    Corrected sample config formatting

--- conf/formats.sample.js
+++ conf/formats.sample.js
@@ -1,12 +1,13 @@
 "use strict";
 module.exports = function() {
     return {
-        metric_path: 'the.path.datadog.uses.for.these.metrics'
-        field_tags: ["name", "status", "full_name", "age"]
+        'key': {
+        metric_path: 'the.path.datadog.uses.for.these.metrics',
+        field_tags: ["name", "status", "full_name", "age"],
         extra_tags: {
             "env": "",
             "user": "",
             "is_test": true
-        }
+        }}
     };
 });
